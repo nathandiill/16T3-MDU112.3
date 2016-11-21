4d0528e8b7ab96a961683aaa50b8f939cf5e1b80
Nathan Diill <Nathan Diill>
2016-11-22 04:39:33 +1000

    Player can move. Background and walls added. Main Menu added with working launch button.

---
+++ 16T3-MDU112.3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D rb;
+    private Vector2 userInput;
+    public float movementSpeed = 5.0f;
+
+	void Start ()
+    {
+        // Retrieve the Ridgidbody from gameObject
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        userInput = new Vector2(horizontal, vertical);
+    }
+
+    void FixedUpdate()
+    {
+        // Call movement in FixedUpdate
+        Vector2 newPosition = transform.position;
+        newPosition += userInput * movementSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
+    }
+}
---
+++ 16T3-MDU112.3/Assets/Scripts/Walls.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walls : MonoBehaviour {
+    private Rigidbody2D rb;
+
+	void Start ()
+    {
+        // Retrieve the Ridgidbody from gameObject
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	void Update ()
+    {
+          	
+	}
+}
