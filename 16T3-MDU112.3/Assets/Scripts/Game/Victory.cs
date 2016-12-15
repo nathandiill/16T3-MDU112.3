3cafdbaebd6cf399c8b7ad89219d1196ca38c728
Nathan Diill <Nathan Diill>
2016-12-16 00:40:50 +1000

    Victory screen added.

---
+++ 16T3-MDU112.3/Assets/Scripts/Game/Victory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        SceneManager.LoadScene(2);
+    }
+}
